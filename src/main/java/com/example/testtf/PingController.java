9dd19a764789896c0ca81db05bd83d6c74949692
arielpineiro <arielsrv@gmail.com>
2026-04-26 11:26:17 +0200

    test

--- src/main/java/com/example/testtf/PingController.java
+++ src/main/java/com/example/testtf/PingController.java
@@ -5,11 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
-
     @GetMapping("/ping")
     public String ping() {
         return "pong";
     }
-
 }
 
