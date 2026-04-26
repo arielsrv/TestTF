1731f3dc6a595dd25a9c821f8c265852bd0ac80c
arielpineiro <arielsrv@gmail.com>
2026-04-26 11:27:32 +0200

    test

--- src/test/java/com/example/testtf/TestTestTfApplication.java
+++ src/test/java/com/example/testtf/TestTestTfApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 
 public class TestTestTfApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.from(TestTfApplication::main).with(TestcontainersConfiguration.class).run(args);
     }
 
